f235576780b361a04cbe6cbc905021fbe60dd913
gary g <tintin2000@yahoo.com>
2013-11-15 15:40:23 -0500

    first commit

---
+++ Recomendo/RCRecommendationMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCRecommendationMapViewController.h
+//  Recomendo
+//
+//  Created by me on 11/15/13.
+//  Copyright (c) 2013 Recomendo Inc 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCRecommendationMapViewController : UIViewController
+
+@end
---
+++ Recomendo/RCRecommendationMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  RCRecommendationMapViewController.m
+//  Recomendo
+//
+//  Created by me on 11/15/13.
+//  Copyright (c) 2013 Recomendo Inc 2013. All rights reserved.
+//
+
+#import "RCRecommendationMapViewController.h"
+
+@interface RCRecommendationMapViewController ()
+
+@end
+
+@implementation RCRecommendationMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
